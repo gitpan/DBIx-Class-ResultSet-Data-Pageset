a17048ca5b256c1d36573a8c93e08add6b599662
Brian Cassidy <bricas@cpan.org>
2008-02-26 12:51:32 +0000

    Import of BRICAS/DBIx-Class-ResultSet-Data-Pageset-0.04 from CPAN.
    gitpan-cpan-distribution: DBIx-Class-ResultSet-Data-Pageset
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         BRICAS/DBIx-Class-ResultSet-Data-Pageset-0.04.tar.gz
    gitpan-cpan-author:       BRICAS
    gitpan-cpan-maturity:     released

--- lib/DBIx/Class/ResultSet/Data/Pageset.pm
+++ lib/DBIx/Class/ResultSet/Data/Pageset.pm
@@ -7,7 +7,7 @@ use base qw( DBIx::Class::ResultSet );
 
 use Data::Pageset ();
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 NAME
 
@@ -15,6 +15,10 @@ DBIx::Class::ResultSet::Data::Pageset - Get a Data::Pageset pager from a results
 
 =head1 SYNOPSIS
 
+    # in your resultsource class
+    __PACKAGE__->resultset_class( 'DBIx::Class::ResultSet::Data::Pageset' );
+    
+    # in your calling code
     my $rs = $schema->resultset('Foo')->search( { }, { pages_per_set => 5 } );
     my $pager = $rs->pageset;
     
@@ -25,6 +29,13 @@ DBIx::Class::ResultSet::Data::Pageset - Get a Data::Pageset pager from a results
 
 This is a simple way to allow you to get a L<Data::Pageset> object for paging
 rather than the standard L<Data::Page> object.
+
+=head1 INSTALLATION
+
+    perl Makefile.PL
+    make
+    make test
+    make install
 
 =head1 METHODS
 
@@ -51,6 +62,10 @@ sub pageset {
         mode          => $attrs->{ pageset_mode }  || 'fixed',
     } );
 }
+
+=head1 AUTHOR
+
+Brian Cassidy E<lt>bricas@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
